86b949ad2c8427d95e80cf163e4f06c15e05e1d3
LANACO\trifkovicv <vladimir.trifkovic@lanaco.com>
2023-05-15 11:14:06 +0200

    Created new table

--- migrations/V1__Initial.sql
+++ migrations/V1__Initial.sql
@@ -4,3 +4,8 @@ CREATE TABLE HelloWorld (
     CONSTRAINT PK_HelloWorld PRIMARY KEY CLUSTERED (HelloWorldId)
 );
 
+CREATE TABLE HelloWorld1 (
+    HelloWorldId INT IDENTITY NOT NULL,
+    Declaration nvarchar(1000) NULL,
+    CONSTRAINT PK_HelloWorld PRIMARY KEY CLUSTERED (HelloWorldId)
+);
